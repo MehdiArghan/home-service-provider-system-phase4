dcd839d1fd77d8bc42dc9cb1c33f5f21cbaf2031
mehdi <mehdiarghan@gmail.com.git/>
2024-06-15 21:40:02 +0330

    created AdminDetails class

---
+++ src/main/java/com/example/homeserviceprovidersystem/security/AdminDetails.java
@@ -0,0 +1,53 @@
+package com.example.homeserviceprovidersystem.security;
+
+import com.example.homeserviceprovidersystem.entity.Admin;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class AdminDetails implements UserDetails {
+    Admin admin;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+    @Override
+    public String getPassword() {
+        return admin.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return admin.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
