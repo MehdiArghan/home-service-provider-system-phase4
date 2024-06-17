43cdeb280f6670cf4df164a7fd036b4c66a6143d
mehdi <mehdiarghan@gmail.com.git/>
2024-06-17 09:43:48 +0330

    added authenticationManager method in SecurityConfiguration

--- src/main/java/com/example/homeserviceprovidersystem/security/SecurityConfiguration.java
+++ src/main/java/com/example/homeserviceprovidersystem/security/SecurityConfiguration.java
@@ -1,12 +1,18 @@
 package com.example.homeserviceprovidersystem.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
