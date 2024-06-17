8c86c00c699c71942a5f964502063b21093cee33
mehdi <mehdiarghan@gmail.com.git/>
2024-06-17 09:39:20 +0330

    Øcanging the implementation of MloadUserByname method in UserDetialsServiceImpl

--- src/main/java/com/example/homeserviceprovidersystem/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/homeserviceprovidersystem/security/UserDetailsServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.homeserviceprovidersystem.security;
 
+import com.example.homeserviceprovidersystem.customeException.CustomEntityNotFoundException;
+import com.example.homeserviceprovidersystem.entity.Admin;
 import com.example.homeserviceprovidersystem.entity.Customer;
 import com.example.homeserviceprovidersystem.entity.Expert;
+import com.example.homeserviceprovidersystem.service.AdminService;
 import com.example.homeserviceprovidersystem.service.CustomerService;
 import com.example.homeserviceprovidersystem.service.ExpertService;
 import lombok.AccessLevel;
@@ -17,20 +20,30 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDetailsServiceImpl implements UserDetailsService {
     public static String userType = "";
+    final AdminService adminService;
     final CustomerService customerService;
     final ExpertService expertService;
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (userType.equals("CUSTOMER")) {
-            Customer customer = customerService.findByEmail(username);
-            return new CustomerDetails(customer);
-        } else if (userType.equals("EXPERT")) {
-            Expert expert = expertService.findByEmail(username);
-            return new ExpertDetails(expert);
-        } else {
-            return null;
+        if (userType == null || userType.isEmpty()) {
+            throw new CustomEntityNotFoundException("User type is not specified");
+        }
+        switch (userType) {
+            case "ADMIN" -> {
+                Admin admin = adminService.findByEmail(username);
+                return new AdminDetails(admin);
+            }
+            case "CUSTOMER" -> {
+                Customer customer = customerService.findByEmail(username);
+                return new CustomerDetails(customer);
+            }
+            case "EXPERT" -> {
+                Expert expert = expertService.findByEmail(username);
+                return new ExpertDetails(expert);
+            }
+            default -> throw new CustomEntityNotFoundException("User type is not specified");
         }
     }
 }
