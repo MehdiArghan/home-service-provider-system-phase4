c4401d13741d306a988a7bdd895fb6961a1eb917
mehdi <mehdiarghan@gmail.com.git/>
2024-06-17 09:46:32 +0330

    added passwordEncoder method in SecurityConfigurtion

--- src/main/java/com/example/homeserviceprovidersystem/security/SecurityConfiguration.java
+++ src/main/java/com/example/homeserviceprovidersystem/security/SecurityConfiguration.java
@@ -6,11 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
